6a4147bd4cb1a8967baa8a35e7aba3c5a57b1eff
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-05 22:11:28 -0600

    datatypes operators lab 4

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -18,6 +18,18 @@ class RelationalOperators {
         }
 
         // write your code below
+        if (a <= b){
+            System.out.println("a is less than or equal to b");
+        }
+        if (a > b){
+            System.out.println("a is greater than b");
+        }
+        if (a >= b){
+            System.out.println("a is greater or equal to b");
+        }
+        if (a == b){
+            System.out.println("a is equal to b");
+        }
 
     }
 
