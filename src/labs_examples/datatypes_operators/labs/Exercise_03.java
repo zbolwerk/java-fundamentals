389b1cd0fec3db23d1d776a9f036061c4088fe15
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-05 22:09:11 -0600

    datatypes operators lab 3

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,19 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int a = 4;
+        int b = 9;
+
+        int c = a + b;
+        System.out.println("value for addition is 13: " + c);
+        int d = b - a;
+        System.out.println("value for subtraction is 5: " + d);
+        int e = a * b;
+        System.out.println("value for multiplication is 36: " + e);
+        int f = b / a;
+        System.out.println("value for division is 2: " + f);
+        int g = b % a;
+        System.out.println("value for modulus is 1: " + g);
 
     }
 
