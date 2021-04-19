6a34ced374502f9ea1457597e6bc485aef61bc4e
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-18 20:43:19 -0600

    Exercise 9 done

--- src/labs_examples/conditions_loops/labs/Exercise_09.java
+++ src/labs_examples/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,15 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            if (i < 99) {
+                if (i % 33 == 3) {
+                    System.out.println("Bingo!");
+                    break;
+                    //break here or you will print out "Bingo!" three times since the condition is met 3 times and it only needs to be met once to print in this scenario.
+                }
+            }
+        }
+    }
 }
