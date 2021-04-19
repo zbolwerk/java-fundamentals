fb5416e7844b091d9af7ab1e679f5445c1438f3d
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-18 20:57:03 -0600

    Exercise 10 done

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,14 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 7) {
+                //i will be 7 when entering and will continue back to the start of the loop thus skipping printing the number 7.
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
