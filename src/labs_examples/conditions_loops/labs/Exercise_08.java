745f105e701d9e93a51bc7ec30b1ee79c8e68a27
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-18 20:36:04 -0600

    Exercise 8 done

--- src/labs_examples/conditions_loops/labs/Exercise_08.java
+++ src/labs_examples/conditions_loops/labs/Exercise_08.java
@@ -8,4 +8,12 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_08 {
+    public static void main(String[] args) {
+        int i = 1000;
+        do {
+            System.out.println(i);
+            i-=3;
+        }
+        while (i >= 1);
+    }
 }
