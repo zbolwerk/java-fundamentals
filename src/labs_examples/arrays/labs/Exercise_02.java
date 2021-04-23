c489d347079d70c38ed9a62ca0170e2fdf600cc5
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-22 19:26:51 -0600

    Complete arrays lab 2

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,4 +1,6 @@
 package labs_examples.arrays.labs;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *  More labs_examples.arrays
@@ -13,9 +15,22 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
+        Scanner scanner = new Scanner(System.in);
         // write code here
 
+        System.out.println("Please enter in a number between 1 and 10.");
+        int userNumber = scanner.nextInt();
+
+        if (userNumber > 0 && userNumber <=10 ) {
+            for (int i = 0; i < array.length; i++) {
+                if (userNumber == array[i]) {
+                    System.out.printf("The index for %s is %s",userNumber, i);
+                }
+            }
+        }
+        else {
+            System.out.println("Invalid number.");
+        }
 
     }
 }
