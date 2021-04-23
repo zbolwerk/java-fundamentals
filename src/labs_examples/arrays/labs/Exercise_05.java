2d779b175ea4ad15c5192e079cefa022ef7e5efe
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-22 20:48:29 -0600

    Complete arrays lab 5

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -1,13 +1,22 @@
 package labs_examples.arrays.labs;
 
 /**
- *  Traversing Arrays Backwards
- *
- *      Please create and populate an array of your choosing. Then, please demonstrate how to print out every other
- *      element in the array in reverse order.
- *
+ * Traversing Arrays Backwards
+ * <p>
+ * Please create and populate an array of your choosing. Then, please demonstrate how to print out every other
+ * element in the array in reverse order.
  */
 
 public class Exercise_05 {
 
+    public static void main(String[] args) {
+        int[] reversePrint = {4, 6, 3, 4, 6, 3, 3, 0, 32, 343, 324};
+        int count = reversePrint.length - 1;
+        for (int i = reversePrint.length - 1; i > 0; i--) {
+            if (count >= 0) {
+                System.out.println(reversePrint[count]);
+                count -= 2;
+            }
+        }
+    }
 }
