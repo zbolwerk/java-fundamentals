ec8b8bdc818ec1a2eb49705fb7a2107e141f3db7
Zach Bolwerk <zach.bolwerk@vertafore.com>
2021-04-22 21:29:56 -0600

    Complete arrays lab 7

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,4 +1,5 @@
 package labs_examples.arrays.labs;
+import java.util.ArrayList;
 
 /**
  *  ArrayLists
@@ -10,4 +11,18 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        ArrayList<String> secondList = new ArrayList<>();
+        cars.add("Porsche");
+        cars.add("Ford");
+        cars.add("Jeep");
+        cars.add("Subaru");
+
+        System.out.println(cars.indexOf("Jeep"));
+
+        secondList = (ArrayList)cars.clone();
+        System.out.println(secondList.indexOf("Subaru"));
+
+    }
 }
