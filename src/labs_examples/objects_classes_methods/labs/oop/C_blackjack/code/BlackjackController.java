19958462d3efbbdcdb1ec2905dda65020fde5e20
zbolwerk <zach.bolwerk@gmail.com>
2021-08-01 22:36:10 -0500

    card class built

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/code/BlackjackController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/code/BlackjackController.java
@@ -2,5 +2,9 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack.code;
 
 public class BlackjackController {
     public static void main(String[] args) {
+        Card oneCard = new Card(0, 11);
+        System.out.println(oneCard);
+        oneCard = new Card(2, 9);
+        System.out.println(oneCard);
     }
 }
